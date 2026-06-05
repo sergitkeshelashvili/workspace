0661562f0e285848c1c2dcaf95b33b03b10f57e3
@Sergi <sergitkeshelashvili@gmail.com>
2026-06-05 16:24:55 +0400

    Refactor sales and revenue SQL report query
    Refactor SQL query for sales and revenue report, improving readability and structure.

--- clickhouse_test/cliclhouse_test.sql
+++ clickhouse_test/cliclhouse_test.sql
@@ -1,80 +1,71 @@
-WITH userbr AS (
-    SELECT b.*
-    FROM ssa.UserBranches AS b
-    INNER JOIN ssa.UMUxx AS u ON u.uxxid = b.uxxid
+-- 1. Sales & Revenue Report
+-- პროდუქტის სრულ იდენტიფიკაციას: შტრიხკოდი, დასახელება, შიდა კოდი და კატეგორიები (ძირითადი ჯგუფი და ქვეკატეგორია)
+-- მომწოდებლი: რომელ მომწოდებელთან ასოცირდება ესა თუ ის გაყიდული საქონელი.
+-- რა ფასში იყიდებოდა პროდუქტი. სულ რა რაოდენობა გაიყიდა (წმინდა გაყიდვები: გაყიდვებს მინუს დაბრუნებები).
+-- ჯამური შემოსავალი (Revenue): რა თანხა შემოვიდა ამ პროდუქტიდან.
+
+WITH FilteredReceipts AS (
+    -- ვფილტრავთ მონაცემებს რომ ram ლიმიტს არ აცდეს
+    SELECT r.receipt_id, r.batch_id, r.optype, r.rec_status, br.pos_num
+    FROM admin_Lake.APEX_axPMARKET_apos_POS_Receipts AS r
+    INNER JOIN admin_Lake.APEX_axPMARKET_apos_POS_Batches AS br ON br.batch_id = r.batch_id
+    WHERE br.opdate BETWEEN '2026-01-01' AND '2026-06-05'
+      AND r.rec_status = 4
 ),
 
-prods AS (
+SalesData AS (
     SELECT
-        pp.rec_type,
-        pp.prodpp_id,
-        pr.BCode,
-        pro.Products_nu,
-        pp.scount,
-        pp.priceg,
-        pr.incode,
-        pp.vg,
-        pp.opdt,
-        cat.Category_nu,
-        ppcat.PPCat_Nu,
-        pp.status,
-        pp.utypeh,
-        r.batch_id,
-        r.optype,
-        r.rec_status,
-        br.pos_num,
-        bp.branch_id,
-        bch.brname,
-        concat(l.cr, ' - ', a.Acc_nu) AS cr
-    FROM apos.POS_ReceiptProds AS pp
-    INNER JOIN apos.POS_Receipts AS r ON pp.receipt_id = r.receipt_id
-    INNER JOIN apos.POS_Batches AS br ON br.batch_id = r.batch_id
-    INNER JOIN apos.POS_BookProp AS bp ON bp.pos_id = br.pos_num
-    LEFT JOIN dbo.Branchs AS bch ON bch.br = bp.branch_id
-    LEFT JOIN userbr AS ub ON bch.br = ub.brid
-    LEFT JOIN ssa.UMUxx AS u ON u.uxxid = ub.uxxid AND isgrant = 1
-    LEFT JOIN dbo.ProdPP AS pr ON pp.prodpp_id = pr.ProdPP_id
-    LEFT JOIN Products AS pro ON pro.Products_id = pr.Products_id
-    LEFT JOIN Category AS cat ON cat.Category_id = pro.Category_id
-    LEFT JOIN PPCat ON ppcat.PPCat_ID = pr.PPCat_id
-    LEFT JOIN tmpt.LastSuppData AS l ON l.prodpp_id = pp.prodpp_id
-    LEFT JOIN Accounts AS a ON a.acc = l.cr
-    WHERE
-        pp.status = 4
-        AND r.rec_status = 4
-        AND (
-            (pp.rec_type = 1 AND r.optype = 1)
-            OR (pp.rec_type = 5 AND r.optype = 2)
-        )
-        AND br.opdate BETWEEN {d1:Date} AND {d2:Date}
-        AND (ifNull({pos:Int32}, 0) = 0 OR br.pos_num = {pos:Int32})
-        AND ({category_id:Nullable(Int32)} IS NULL OR pro.Category_id = {category_id:Int32})
-        AND ({ppcat_id:Nullable(Int32)} IS NULL OR pr.ppcat_id = {ppcat_id:Int32})
-        AND (
-            {br:Nullable(Int32)} IS NULL
-            OR br.pos_num IN (
-                SELECT pos_id FROM apos.POS_BookProp WHERE branch_id = {br:Int32}
-            )
-        )
+        pp.prodpp_id AS product_id,
+        pr.BCode AS barcode,
+        pro.Products_nu AS product_name,
+        pp.scount AS quantity_sold,
+        pp.priceg AS retail_price,
+        pr.InCode AS internal_code,
+        pp.vg AS sales_revenue,
+        cat.Category_nu AS main_category,
+        pc.PPCat_Nu AS sub_category,
+        concat(l.cr, ' - ', a.Acc_nu) AS supplier_info
+    FROM admin_Lake.APEX_axPMARKET_apos_POS_ReceiptProds AS pp
+    INNER JOIN FilteredReceipts AS r ON pp.receipt_id = r.receipt_id
+
+    LEFT JOIN admin_Lake.APEX_axPMARKET_dbo_ProdPP AS pr ON pp.prodpp_id = pr.ProdPP_id
+    LEFT JOIN admin_Lake.APEX_axPMARKET_dbo_Products AS pro ON pro.Products_id = pr.Products_id
+    LEFT JOIN admin_Lake.APEX_axPMARKET_dbo_Category AS cat ON cat.Category_id = pro.Category_id
+    LEFT JOIN admin_Lake.APEX_axPMARKET_dbo_PPCat AS pc ON pc.PPCat_ID = pr.PPCat_id
+    LEFT JOIN admin_Lake.APEX_axPMARKET_tmpt_LastSuppData AS l ON l.prodpp_id = pp.prodpp_id
+    LEFT JOIN admin_Lake.APEX_axPMARKET_dbo_Accounts AS a ON a.Acc = l.cr
+    WHERE pp.status = 4
+      AND (
+          (pp.rec_type = 1 AND r.optype = 1)
+          OR (pp.rec_type = 5 AND r.optype = 2)
+      )
 )
 
+-- 2. საბოლოო აგრეგაცია შესაბამისი ალიასებით
 SELECT
-    ifNull(BCode, toString(prodpp_id)) AS bcode,
-    Products_nu,
-    incode,
-    Category_nu,
-    PPCat_Nu,
-    sum(scount)                        AS scount,
-    priceg,
-    sum(vg)                            AS total_vg,
-    cr
-FROM prods
+    ifNull(barcode, toString(product_id)) AS Barcode,
+    product_name                          AS ProductName,
+    internal_code                         AS InternalCode,
+    main_category                         AS MainCategory,
+    sub_category                          AS SubCategory,
+    retail_price                          AS RetailPrice,
+    supplier_info                         AS SupplierInfo,
+    sum(quantity_sold)                    AS TotalQuantitySold,
+    sum(sales_revenue)                    AS TotalRevenue
+FROM SalesData
 GROUP BY
-    ifNull(BCode, toString(prodpp_id)),
-    Products_nu,
-    Category_nu,
-    PPCat_Nu,
-    priceg,
-    incode,
-    cr
-SETTINGS join_use_nulls = 1
+    ifNull(barcode, toString(product_id)),
+    product_name,
+    internal_code,
+    main_category,
+    sub_category,
+    retail_price,
+    supplier_info
+ORDER BY TotalRevenue DESC
+
+SETTINGS
+    join_use_nulls = 1,
+    -- თუ მეხსიერება არ ეყო, ClickHouse დააჯგუფებს დისკზე
+    max_bytes_before_external_group_by = 20000000000,
+    -- ჯოინების ოპტიმიზაციისთვის დისკის გამოყენება
+    max_bytes_before_external_sort = 10000000000;
